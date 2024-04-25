db4da5091a17ea986f1d0c5539a47a447282122d
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-25 18:30:05 -0300

    .

--- src/main/java/com/ccp/jn/vis/async/business/factory/VisAsyncBusinessFactory.java
+++ src/main/java/com/ccp/jn/vis/async/business/factory/VisAsyncBusinessFactory.java
@@ -7,10 +7,9 @@ import java.util.function.Function;
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.especifications.async.business.factory.CcpAsyncBusinessFactory;
 import com.ccp.jn.vis.async.business.positions.VisAsyncBusinessPositionInactivate;
-import com.ccp.jn.vis.async.business.positions.VisAsyncBusinessPositionSchedulling;
 import com.ccp.jn.vis.async.business.positions.VisAsyncBusinessPositionSendResume;
 import com.ccp.jn.vis.async.business.resume.VisAsyncBusinessResumeInactivate;
-import com.ccp.jn.vis.async.business.resume.VisAsyncBusinessResumeSave;
+import com.ccp.jn.vis.async.business.resume.VisAsyncBusinessResumeSendToRecruiters;
 import com.ccp.jn.vis.async.business.resume.VisAsyncBusinessResumeSaveFile;
 import com.jn.vis.commons.utils.VisTopics;
 
@@ -21,12 +20,11 @@ class VisAsyncBusinessFactory implements CcpAsyncBusinessFactory {
 	private Map<String, Function<CcpJsonRepresentation, CcpJsonRepresentation>> map = new HashMap<>();
 	
 	public VisAsyncBusinessFactory() {
+		this.map.put(VisTopics.sendResumeToRecruiters.name(), new VisAsyncBusinessResumeSendToRecruiters());
 		this.map.put(VisTopics.sendResumesToThisPosition.name(), new VisAsyncBusinessPositionSendResume());
 		this.map.put(VisTopics.inactivatePosition.name(), new VisAsyncBusinessPositionInactivate());
 		this.map.put(VisTopics.inactivateResume.name(), new VisAsyncBusinessResumeInactivate());
-		this.map.put(VisTopics.savePosition.name(), new VisAsyncBusinessPositionSchedulling());
 		this.map.put(VisTopics.saveResumeFile.name(), new VisAsyncBusinessResumeSaveFile());
-		this.map.put(VisTopics.saveResume.name(), new VisAsyncBusinessResumeSave());
 	}
 
 	public Map<String, Function<CcpJsonRepresentation, CcpJsonRepresentation>> getMap() {
--- src/main/java/com/ccp/jn/vis/async/business/resume/VisAsyncBusinessResumeSendToRecruiters.java
+++ src/main/java/com/ccp/jn/vis/async/business/resume/VisAsyncBusinessResumeSendToRecruiters.java
@@ -7,13 +7,10 @@ import com.ccp.constantes.CcpConstants;
 import com.ccp.decorators.CcpJsonRepresentation;
 import com.ccp.jn.vis.business.utils.PositionSendFrequency;
 import com.ccp.jn.vis.business.utils.VisAsyncUtils;
-import com.jn.vis.commons.entities.VisEntityResume;
 
-public class VisAsyncBusinessResumeSave implements Function<CcpJsonRepresentation, CcpJsonRepresentation> {
+public class VisAsyncBusinessResumeSendToRecruiters implements Function<CcpJsonRepresentation, CcpJsonRepresentation> {
 
 	public CcpJsonRepresentation apply(CcpJsonRepresentation resume) {
-		// Vis.1.1
-		VisEntityResume.INSTANCE.createOrUpdate(resume);
 		VisAsyncUtils.sendFilteredResumesByEachPositionToEachRecruiter(CcpConstants.EMPTY_JSON.put("frequency", PositionSendFrequency.minute), x -> Arrays.asList(resume));
 		
 		return CcpConstants.EMPTY_JSON;
